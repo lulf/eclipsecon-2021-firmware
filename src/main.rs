0a27aee212b6aba24d3ceb236a8964c9f2fe811d
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-08-18 14:48:59 +0200

    Use new drogue-wasm crate

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,17 @@ mod app;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "wasm")] {
-        mod system;
-        mod components;
-
-        use components::*;
-
         use wasm_bindgen::prelude::*;
         use wasm_bindgen_futures::spawn_local;
+        use drogue_wasm::components::*;
+        use drogue_wasm::system::*;
 
     } else if #[cfg(feature = "microbit")] {
         use panic_probe as _;
         use rtt_logger::RTTLogger;
         use rtt_target::rtt_init_print;
+        use log::LevelFilter;
+        use embedded_hal::digital::v2::OutputPin;
 
         use embassy_nrf::{
             gpio::{AnyPin, Input, Level, NoPin, Output, OutputDrive, Pin, Pull},
@@ -72,7 +71,6 @@ static DEVICE: DeviceContext<MyDevice> = DeviceContext::new();
 #[cfg(feature = "microbit")]
 #[embassy::main]
 async fn main(spawner: embassy::executor::Spawner, p: Peripherals) {
-    use log::LevelFilter;
     rtt_init_print!();
     log::set_logger(&LOGGER).unwrap();
     log::set_max_level(log::LevelFilter::Trace);
@@ -97,7 +95,10 @@ async fn main(spawner: embassy::executor::Spawner, p: Peripherals) {
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
-    let spawner = system::WasmSpawner::new();
+    let spawner = WasmSpawner::new();
+
+    static mut INPUT1: InputPin = InputPin::new();
+    static mut OUTPUT1: OutputPin = OutputPin::new();
 
     // Configure HTML elements
     unsafe {
